a774775f88e61ef431014aa05b35b23f793235cf
Maersel <marcelqualm1@gmail.com>
2022-11-22 13:42:05 +0100

    ubhhbjhbg
    ihjbhhbu

---
+++ boebot/src/BoeBot.java
@@ -0,0 +1,9 @@
+public class BoeBot {
+
+    public BoeBot() {
+    }
+
+    public static void demo1() {
+
+    }
+}
---
+++ boebot/src/Main.java
@@ -0,0 +1,9 @@
+import TI.BoeBot;
+import TI.PinMode;
+import TI.Timer;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
