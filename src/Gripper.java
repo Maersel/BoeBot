10ea72fc895c7ea0af238a432bd1fd4609f4b80c
Nikulair <niekhoevenaars@gmail.com>
2022-11-24 11:06:08 +0100

    gripper v1

--- src/Gripper.java
+++ src/Gripper.java
@@ -1,8 +1,27 @@
+import TI.*;
+
 public class Gripper {
 
-    private int inputPin;
+    private final Servo servo;
 
     public Gripper(int inputPin) {
-        this.inputPin = inputPin;
+        this.servo = new Servo(inputPin);
     }
+
+    private void open(){
+        for (int i = 1325; i <= 1850; i += 3) {
+            servo.update(i);
+            BoeBot.wait(5);
+            System.out.println(i);
+        }
+    }
+
+    private void close(){
+        for (int i = 1850; i >= 1325; i -= 3) {
+            servo.update(i);
+            BoeBot.wait(5);
+            System.out.println(i);
+        }
+    }
+
 }
