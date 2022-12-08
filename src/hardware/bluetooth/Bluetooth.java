808162742e3d988e00196ebf8f366b5b778803ea
Nikulair <niekhoevenaars@gmail.com>
2022-12-08 13:29:24 +0100

    NiBot v1.2 p

--- src/hardware/bluetooth/Bluetooth.java
+++ src/hardware/bluetooth/Bluetooth.java
@@ -1,15 +1,20 @@
 package hardware.bluetooth;
 
 import TI.SerialConnection;
+import TI.Timer;
 import controllers.Configuration;
 import controllers.MovementController;
+import controllers.StateController;
+import hardware.CheckState;
+import hardware.led.NeoPixel;
 
-import javax.swing.plaf.nimbus.State;
 
 public class Bluetooth implements CheckState {
 
     private final SerialConnection serial;
     private final MovementController movementController;
+    private final StateController stateController;
+    private final Callback callback;
 
 
     private final int forwardKey = 'e';
@@ -28,92 +33,61 @@ public class Bluetooth implements CheckState {
 
     Timer t = new Timer(100);
 
-    public Bluetooth(SerialConnection serial, MovementController movementController,
-                     StateController stateController, Callback callback) {
+    public Bluetooth(SerialConnection serial, MovementController movementController, StateController stateController, Callback callback)
+    {
         this.serial = serial;
         this.movementController = movementController;
-        t.mark();
         this.stateController = stateController;
         this.callback = callback;
+        t.mark();
     }
 
-
-    public void echoCode() {
-        if (serial.available() > 0) {
-            int data = serial.readByte();
-            serial.writeByte(data); // Echo data
-            System.out.println("Received: " + data);
-        }
-    }
-
-    public void remote(){
+    public void remote() {
 
         if (serial.available() > 0) {
             int data = serial.readByte();
 
             t.mark();
 
-            if (data == forwardKey)
-            {
+            if (data == forwardKey) {
                 movementController.forward();
                 System.out.println("forward");
-            }
-            else if (data == backwardKey)
-            {
+            } else if (data == backwardKey) {
                 movementController.backwards();
                 System.out.println("backwards");
-            }
-            else if (data == correctLeftKey)
-            {
+            } else if (data == correctLeftKey) {
                 movementController.correctLeft();
                 System.out.println("correctLeft");
-            }
-            else if (data == correctRightKey)
-            {
+            } else if (data == correctRightKey) {
                 movementController.correctRight();
                 System.out.println("correctRight");
-            }
-            else if (data == boosyKey)
-            {
+            } else if (data == boosyKey) {
                 movementController.boosy();
                 System.out.println("boosy");
-            }
-            else if (data == turnLeftKey) {
+            } else if (data == turnLeftKey) {
                 movementController.turnLeft();
                 System.out.println("turnLeft");
-            }
-            else if (data == rightTurnKey) {
+            } else if (data == rightTurnKey) {
                 movementController.turnRight();
                 System.out.println("turnRight");
-            }
-            else if (data == lineFollowModeChangeKey)
-            {
+            } else if (data == lineFollowModeChangeKey) {
                 lineFollowMode = !lineFollowMode;
                 System.out.println("lineFollowModeChange");
-            }
-            else if (data == neoPixeleKey)
-            {
+            } else if (data == neoPixeleKey) {
                 neopixel1 = !neopixel1;
                 NeoPixel.setColour(1f, neopixel1);
                 System.out.println("neoPixeleKey");
-            }
-            else if (data == neoPixeleKey2)
-            {
+            } else if (data == neoPixeleKey2) {
                 neopixel2 = !neopixel2;
                 NeoPixel.setColour(0.5f, neopixel2);
                 System.out.println("neoPixeleKey");
-            }
-            else{
+            } else {
                 movementController.stop();
                 System.out.println("stop");
             }
-        }
-        else if (lineFollowMode)
-        {
+        } else if (lineFollowMode) {
 
-        }
-        else
-        {
+        } else {
             if (t.timeout()) {
                 movementController.stop();
                 System.out.println("stop");
